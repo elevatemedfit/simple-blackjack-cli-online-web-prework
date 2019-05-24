b8b5a935ae1c98ca74aa66be5bdfc69798d34f0e
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-24 15:24:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,7 @@ def hit?(sum)
       prompt_user
       responce=get_user_input
       if responce=='h'
-        deal_card
+        card_total+=deal_card
       elsif responce!='h'&&responce!='s'
         invalid_command
         prompt_user
